f518921cf2da9e7fd758f72a9c3be47021f0b917
anthony <anthonyspiteri@hotmail.fr>
2022-11-14 15:22:38 +0100

    css et js mis à jour

--- js/index.js
+++ js/index.js
@@ -1,3 +1,25 @@
-console.log('Hello');
-console.log('teste anthony')
-console.log('coucou mehdi')
+const todo = document.getElementById('app'); // ma div
+const todoTitle = document.createElement('h1'); // titre todolist
+const todoList = document.createElement('ul'); // la liste 
+
+const todoItemList = ["faire les courses", "faire le ménage", "apprendre le js", "apprendre à utilise git","passer gold sur lol"]; // liste des taches à faire
+
+
+todo.append(todoTitle); // j'attache mon h1 à ma div
+todo.append(todoList);  // j'attache mon ul à ma div
+
+todoTitle.innerText = "TODO LIST"; // Mon h1
+todoItemList.map( element => {
+    const item = document.createElement('li');
+    const myButton = document.createElement('button');
+const itemContent = document.createElement('p');
+    todoList.append(item) // j'attache mon li à mon ul
+    item.append(itemContent, myButton)
+    itemContent.innerText = element.charAt(0).toUpperCase() + element.slice(1); // 1er lettre de la liste en majuscule
+    myButton.innerText = 'Delete'; // mon bouton pour supprimer les tàches
+
+    myButton.addEventListener('click', function(){ // fonction pour supprimer le bouton
+        item.remove()
+        })
+})
+
